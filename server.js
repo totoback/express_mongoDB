bf99e28539cd73278f8516793087f2a6023652db
지영 <toto.joung@gmail.com>
2023-08-13 17:27:42 +0900

    230813_mongodb연결 완

--- server.js
+++ server.js
@@ -1,31 +1,22 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const MongoClient = require("mongodb").MongoClient;
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config(); //env쓰기위해 가져옴
 
 const PORT = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-MongoClient.connect(
-  "mongodb+srv://totojoung:dkssud@boilerplate.s8nobyi.mongodb.net/",
-  function (에러, client) {
-    if (에러) return console.log(에러);
-    //서버띄우는 코드 여기로 옮기기
-    app.listen(PORT, () => {
-      console.log(`${PORT}번에서 서버 실행중!`);
-    });
-  }
-);
-
-// mongoose
-//   .connect(process.env.MONGO_URI) //몽고디비 연결 URI ENV연결
-//   .then(() => { //성공
-//     console.log("연결 완료");
-//   })
-//   .catch((err) => { //실패
-//     console.error(err);
-//   });
+mongoose
+  .connect(process.env.MONGO_DB_URI) //몽고디비 연결 URI ENV연결
+  .then(() => { //성공
+    console.log("연결 완료");
+  })
+  .catch((err) => { //실패
+    console.error(err);
+  });
 
 app.get("/", (req, res) => {
   //html 파일 보내주는 방법
@@ -41,4 +32,8 @@ app.post("/add", (req, res) => {
   res.send("전송 완료");
   const data = req.body;
   console.log(data);
+});
+
+app.listen(PORT, () => {
+  console.log(`${PORT}번에서 서버 실행중!`);
 });
