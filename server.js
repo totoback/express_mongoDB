536d8d7d0bd0fe16969770cb38f2a00a216048e0
지영 <toto.joung@gmail.com>
2023-08-13 17:34:27 +0900

    mongodb 버전 변경

--- server.js
+++ server.js
@@ -1,22 +1,32 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config(); //env쓰기위해 가져옴
+const MongoClient = require('mongodb').MongoClient;
 
 const PORT = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose
-  .connect(process.env.MONGO_DB_URI) //몽고디비 연결 URI ENV연결
-  .then(() => { //성공
-    console.log("연결 완료");
-  })
-  .catch((err) => { //실패
-    console.error(err);
+// mongoose
+//   .connect(process.env.MONGO_DB_URI) //몽고디비 연결 URI ENV연결
+//   .then(() => { //성공
+//     console.log("연결 완료");
+//   })
+//   .catch((err) => { //실패
+//     console.error(err);
+//   });
+
+MongoClient.connect(process.env.MONGO_DB_URI, function (error, client) {
+  //연결되면 할일
+  if (error) return console.log(error);
+  //서버띄우는 코드 여기로 옮기기
+  app.listen(PORT, () => {
+    console.log(`${PORT}번에서 서버 실행중!`);
   });
+});
 
 app.get("/", (req, res) => {
   //html 파일 보내주는 방법
@@ -34,6 +44,4 @@ app.post("/add", (req, res) => {
   console.log(data);
 });
 
-app.listen(PORT, () => {
-  console.log(`${PORT}번에서 서버 실행중!`);
-});
+
